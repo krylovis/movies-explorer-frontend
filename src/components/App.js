c554dc75cd8bcdacb7c697e42c8abf3a4a78b36e
Ivan Krylov <kis.226@yandex.ru>
2023-07-30 11:39:24 +0300

    default settings

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Route, Routes, useNavigate } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
