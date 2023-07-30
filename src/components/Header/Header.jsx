76fc529df1d7a51284a379ec02ceaad6eceaa688
Ivan Krylov <kis.226@yandex.ru>
2023-07-30 15:15:44 +0300

    add header__container

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../../images/logo.svg';
+import { Routes, Route, NavLink, useNavigate } from 'react-router-dom';
+
+export default function Header() {
+
+  return (
+    <div className='header__container'></div>
+  )
+}
