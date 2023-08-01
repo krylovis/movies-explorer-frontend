a676ca732e35d64343c902d7b0c8ce856aff767e
Krylov Ivan <krylovis@mchs.ru>
2023-08-01 11:30:40 +0300

    add menuButton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import { Routes, Route, NavLink, useNavigate, useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import logo from '../../images/logo.svg';
 
 export default function Header(props) {
-  const { loggedIn = true } = props;
-  // const [isMobile, setIsMobile] = React.useState(false);
+  const { loggedIn = false } = props;
+
+  const [isMobile, setIsMobile] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   const { pathname } = useLocation();
   const isResultLanding = pathname === '/';
 
   const headerClassName = `header ${isResultLanding ? 'header_type_main-page' : ''}`;
   const linksContainerClassName = `header__links-container ${loggedIn ? 'header__links-container_type_logged' : ''}`;
+
+  React.useEffect(() => {
+    const resize = () => {
+      const mobileWidth = document.body.clientWidth <= 768;
+      if (mobileWidth) {
+        setIsMobile(true);
+      } else {
+        setIsOpen(false);
+        setIsMobile(false);
+      }
+    };
+    resize();
+    window.addEventListener('resize', resize);
+    return () => window.removeEventListener("resize", resize);
+  }, [setIsMobile]);
+
+  const toggleMenu = () => setIsOpen(isOpen => !isOpen);
+
+  const menuButtonClassName = `button header__menu-button ${isOpen ? 'header__menu-button_type_closed' : ''}`;
 
   const navLinks = () => {
     if (loggedIn) {
@@ -39,16 +60,31 @@ export default function Header(props) {
     }
   };
 
+  const menuButton = () => {
+    return (
+      <button
+        className={menuButtonClassName}
+        type="button"
+        title={isOpen ? 'Закрыть меню' : 'Открыть меню'}
+        aria-label="Кнопка меню"
+        onClick={toggleMenu}
+      />
+    )
+  };
+
   return (
     <header className={headerClassName}>
       <div className="header__container">
-        <NavLink to="/">
+        <NavLink className="header__link-logo" to="/">
           <img src={logo} alt="Логотип: Movies Explorer" title="Главная страница" className="header__logo" />
         </NavLink>
 
-        <nav className={linksContainerClassName}>
-          {navLinks()}
-        </nav >
+        {(isMobile) && menuButton()}
+
+        {(!isMobile) &&
+          <nav className={linksContainerClassName}>
+            {navLinks()}
+          </nav >}
       </div>
     </header>
   )
