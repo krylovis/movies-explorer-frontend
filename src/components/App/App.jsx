d9f4d5c9920e33f0abf40481981eac0deff15f08
Krylov Ivan <krylovis@mchs.ru>
2023-08-01 09:58:44 +0300

    add pathsForHeader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-// import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
 
+
 export default function App() {
+  const pathsForHeader = ["/", "/movies", "/saved-movies", "/profile"];
+
   return (
-    <Header />
+    <Routes>
+      {pathsForHeader.map((path) => <Route key={path} path={path} element={<Header />} />)}
+    </Routes>
   );
 }
