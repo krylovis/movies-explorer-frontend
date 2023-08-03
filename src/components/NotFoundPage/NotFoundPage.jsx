1565da906db8531bca3470fb7853d7de814f5774
Krylov Ivan <krylovis@mchs.ru>
2023-08-03 11:20:30 +0300

    fix NotFoundPage

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -5,10 +5,12 @@ export default function NotFoundPage() {
   return (
     <section className="not-found-page">
       <div className="not-found-page__container">
-        <span className="not-found-page__error">404</span>
-        <span className="not-found-page__text">Страница не найдена</span>
+        <div className="not-found-page__error-container">
+          <span className="not-found-page__error">404</span>
+          <span className="not-found-page__text">Страница не найдена</span>
+        </div>
+        <NavLink className="link not-found-page__link" to="/">Назад</NavLink>
       </div>
-      <NavLink className="link not-found-page__link" to="/">Назад</NavLink>
     </section>
   )
 }
