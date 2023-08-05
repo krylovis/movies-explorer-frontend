242902b1cf1749d82526b445cfcf82eb46e63691
Ivan Krylov <kis.226@yandex.ru>
2023-08-05 14:08:05 +0300

    add handleChange

--- src/components/auth/SigninPage/SigninPage.jsx
+++ src/components/auth/SigninPage/SigninPage.jsx
@@ -1,14 +1,21 @@
 import AuthContainer from '../AuthContainer/AuthContainer';
+import InputTypeEmail from '../../inputs/InputTypeEmail';
+import InputTypePassword from '../../inputs/InputTypePassword';
+
+import { useForm } from '../../../hooks/useForm';
 
 // авторизация
 export default function SigninPage() {
+  const { values, handleChange } = useForm({ email: '', password: '' });
   return (
     <AuthContainer
       authTitle="Рады видеть!"
       formName="signin"
       buttonText="Войти"
     >
-      {/* SigninPage */}
+      <InputTypeEmail values={values} handleChange={handleChange} />
+      <InputTypePassword values={values} handleChange={handleChange} />
+
     </AuthContainer>
   )
 }
