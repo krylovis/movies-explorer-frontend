3545dcbd7db162e16de9168167b8a1db144b50ae
Ivan Krylov <kis.226@yandex.ru>
2023-08-05 14:06:26 +0300

    add InputTypePassword

---
+++ src/components/inputs/InputTypePassword.jsx
@@ -0,0 +1,19 @@
+export default function InputTypePassword(props) {
+  const { values, handleChange } = props;
+  return (
+    <label className="input__label" htmlFor="inputTypePassword">
+      <input
+        className="input input_password"
+        id="inputTypePassword"
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+        minLength="6"
+        maxLength="200"
+        required
+      />
+      <span className="input__error-message inputTypePassword-error"></span>
+    </label>
+  )
+}
