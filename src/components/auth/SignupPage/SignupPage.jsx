43b8a79b0d7d21e80c4193d99374b914efb5884d
Ivan Krylov <kis.226@yandex.ru>
2023-08-05 14:20:08 +0300

    add inputs for signup

--- src/components/auth/SignupPage/SignupPage.jsx
+++ src/components/auth/SignupPage/SignupPage.jsx
@@ -1,14 +1,33 @@
+// import { useNavigate } from 'react-router-dom';
+
 import AuthContainer from '../AuthContainer/AuthContainer';
+import InputTypeEmail from '../../inputs/InputTypeEmail';
+import InputTypePassword from '../../inputs/InputTypePassword';
+import InputTypeName from '../../inputs/InputTypeName';
+
+import { useForm } from '../../../hooks/useForm';
 
 // регистрация
 export default function SignupPage() {
+  const { values, handleChange } = useForm({ name: '', email: '', password: '' });
+  // const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('values', values);
+  };
+
   return (
     <AuthContainer
+      onSubmit={handleSubmit}
       authTitle="Добро пожаловать!"
       formName="signup"
       buttonText="Зарегистрироваться"
     >
-      {/* SignupPage */}
+      <InputTypeName values={values} handleChange={handleChange} />
+      <InputTypeEmail values={values} handleChange={handleChange} />
+      <InputTypePassword values={values} handleChange={handleChange} />
+
     </AuthContainer>
   )
 }
