d9b0a591798a371c3af4e4a024f88ea7990c0f53
Ivan Krylov <kis.226@yandex.ru>
2023-08-05 14:06:07 +0300

    add InputTypeEmail

---
+++ src/components/inputs/InputTypeEmail.jsx
@@ -0,0 +1,22 @@
+import { emailPattern } from '../../utils/constants';
+
+export default function InputTypeEmail(props) {
+  const { values, handleChange } = props;
+  return (
+    <label className="input__label" htmlFor="inputTypeEmail">
+      <input
+        className="input input_email"
+        id="inputTypeEmail"
+        type="email"
+        name="email"
+        pattern={emailPattern}
+        value={values.email}
+        onChange={handleChange}
+        minLength="2"
+        maxLength="40"
+        required
+      />
+      <span className="input__error-message inputTypeEmail-error"></span>
+    </label>
+  )
+}
