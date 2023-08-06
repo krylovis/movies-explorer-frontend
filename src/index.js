87c1954f2310538ad6ff4fbeffd51c66492d342c
Ivan Krylov <kis.226@yandex.ru>
2023-08-06 10:15:07 +0300

    rename

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const app = ReactDOM.createRoot(document.querySelector('#app'));
+app.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
