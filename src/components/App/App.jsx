1d0e0a49bbebdac386846300fc0ac7cee21d3aed
Krylov Ivan <krylovis@mchs.ru>
2023-08-09 09:37:43 +0300

    add css for promo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 import Main from '../Main/Main';
+import AboutProject from '../AboutProject/AboutProject';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import ProfilePage from '../ProfilePage/ProfilePage';
@@ -19,6 +20,7 @@ export default function App() {
       <Routes>
 
         <Route path='/' element={<Main />} >
+          <Route path='/' element={<AboutProject />} />
           <Route path='/movies' element={<Movies />} />
           <Route path='/saved-movies' element={<SavedMovies />} />
           <Route path='/profile' element={<ProfilePage />} />
---
+++ src/components/Promo/Promo.jsx
@@ -0,0 +1,9 @@
+export default function Promo() {
+  return (
+    <section className="promo">
+      <div className="promo__container">
+        <h1 className="promo__title">Учебный&nbsp;проект студента факультета Веб&#8209;разработки.</h1>
+      </div>
+    </section>
+  )
+}
