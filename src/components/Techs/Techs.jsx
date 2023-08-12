0eabe7a9536fa9d5b7da73e9741e8e236b9af7c4
Ivan Krylov <kis.226@yandex.ru>
2023-08-12 12:00:02 +0300

    add techs content

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -3,10 +3,26 @@ import AboutContainer from '../AboutContainer/AboutContainer';
 export default function Techs() {
   const title = 'Технологии';
   const type = 'techs';
+  const techsList = ['HTML', 'CSS', 'JS', 'React', 'Git', 'Express.js', 'mongoDB'];
+  const techsTitle = `${techsList.length} технологий`;
+
+  const techsItems = techsList.map((techsItem, index) =>
+    <li key={index} className="techs__item">{techsItem}</li>);
 
   return (
     <AboutContainer title={title} type={type}>
-      <div className="techs"></div>
+      <div className="techs">
+
+        <div className="techs__info">
+          <h3 className="techs__title">{techsTitle}</h3>
+          <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        </div>
+
+        <ul className="list techs__list">
+          {techsItems}
+        </ul>
+
+      </div>
     </AboutContainer>
   )
 }
