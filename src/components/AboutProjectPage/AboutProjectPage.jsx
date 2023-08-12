ba7b20e0c1f1cb16827f546a5577eff7b0ae16a4
Ivan Krylov <kis.226@yandex.ru>
2023-08-12 11:01:49 +0300

    add Techs section

--- src/components/AboutProjectPage/AboutProjectPage.jsx
+++ src/components/AboutProjectPage/AboutProjectPage.jsx
@@ -1,11 +1,12 @@
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 export default function AboutProjectPage() {
   return (
     <section className="about-project-page">
       <Promo />
       <AboutProject />
-    </section>
+      <Techs />
   )
 }
---
+++ src/components/Techs/Techs.jsx
@@ -0,0 +1,11 @@
+import AboutContainer from '../AboutContainer/AboutContainer';
+
+export default function Techs() {
+  const title = 'Технологии';
+
+  return (
+    <AboutContainer title={title}>
+      <div className="techs"></div>
+    </AboutContainer>
+  )
+}
