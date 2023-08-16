64dfed91842ed153a41434945657c8401ca8a6df
Krylov Ivan <krylovis@mchs.ru>
2023-08-16 09:17:28 +0300

    fix

--- src/components/SectionContainer/SectionContainer.jsx
+++ src/components/SectionContainer/SectionContainer.jsx
@@ -3,11 +3,14 @@ export default function SectionContainer(props) {
 
   const containerClass = `section-container ${type ? `section-container_${type}` : ''}`;
   const wrapperClass = `section-container__wrapper ${type ? `section-container__wrapper_${type}` : ''}`;
+
   return (
     <section className={containerClass}>
+
       <div className={wrapperClass}>
         {children}
       </div>
+
     </section>
   )
 }
