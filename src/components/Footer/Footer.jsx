36cbb34259e6c31abf8c4485097adb61a8753f26
Krylov Ivan <krylovis@mchs.ru>
2023-08-16 11:01:00 +0300

    fix footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <h4 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h4>
+        <h4 className="footer__title footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</h4>
         <div className="footer__nav-container">
-          <span className="footer__date">© {fullYear}</span>
+          <span className="footer__date footer__text">© {fullYear}</span>
           <nav className="footer__links">
-            <a href="https://practicum.yandex.ru/" target="_blank" className="link footer__link" rel="noreferrer">Яндекс.Практикум</a>
-            <a href="https://github.com/" target="_blank" className="link footer__link" rel="noreferrer">Github</a>
+            <a href="https://practicum.yandex.ru/" target="_blank" className="link footer__link footer__text" rel="noreferrer">Яндекс.Практикум</a>
+            <a href="https://github.com/" target="_blank" className="link footer__link footer__text" rel="noreferrer">Github</a>
           </nav>
         </div>
       </div>
