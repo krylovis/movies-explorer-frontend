9e727f02d3a7d2bd9fb6e65b7a7611477b8f794b
Krylov Ivan <krylovis@mchs.ru>
2023-08-17 15:50:23 +0300

    fix 320 css

--- src/components/AboutContainer/AboutContainer.jsx
+++ src/components/AboutContainer/AboutContainer.jsx
@@ -2,12 +2,13 @@ export default function AboutContainer(props) {
   const { title, type, children } = props;
   const aboutContainerClass = `about-container ${type ? `about-container_type_${type}` : ''}`;
   const contentClass = `about-container__content ${type ? `about-container__content_type_${type}` : ''}`;
+  const titleClass = `about-project__title ${type ? `about-project__title_type_${type}` : ''}`;
 
   return (
     <section className={aboutContainerClass}>
 
       <div className={contentClass}>
-        <h2 className="about-project__title">{title}</h2>
+        <h2 className={titleClass}>{title}</h2>
 
         {children}
       </div>
