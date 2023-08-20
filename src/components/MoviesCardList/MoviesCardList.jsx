15f90d9421b0a2b5a94a4fd9e76c662933c9fa74
Ivan Krylov <kis.226@yandex.ru>
2023-08-20 12:09:49 +0300

    add more button and fix css

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -5,15 +5,28 @@ import { cardList } from '../../utils/cardList';
 export default function MoviesCardList() {
 
   const toggleCardLike = () => console.log('toggleCardLike');
+  const showMoreMovies = () => console.log('moreFilms');
 
   return (
     <SectionContainer type="type_movies-list">
-      {cardList.map((card) => (
-        <MoviesCard key={card._id}
-          card={card}
-          onBtnClick={toggleCardLike}
-        />
-      ))}
+      <div className="type_movies-list__list">
+        {cardList.map((card) => (
+          <MoviesCard key={card._id}
+            card={card}
+            onBtnClick={toggleCardLike}
+          />
+        ))}
+      </div>
+
+      <button
+        className="button type_movies-list__button"
+        type="button"
+        aria-label="Показать больше фильмов"
+        title="Показать больше фильмов"
+        onClick={showMoreMovies}
+      >
+        Ещё
+      </button>
     </SectionContainer>
   )
 }
