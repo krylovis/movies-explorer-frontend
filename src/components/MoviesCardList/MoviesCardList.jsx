1d41dc3cbe61cfd4339114dae32f57ba65d345d3
Ivan Krylov <kis.226@yandex.ru>
2023-08-20 11:20:14 +0300

    add test cardList

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,17 +1,8 @@
 import SectionContainer from '../../components/SectionContainer/SectionContainer';
 import MoviesCard from '../MoviesCard/MoviesCard';
+import { cardList } from '../../utils/cardList';
 
 export default function MoviesCardList() {
-
-  const cardList = [
-    {
-      _id: 1,
-      poster: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/d7e3dbd6-e4a9-4485-b751-d02f49825166/300x450',
-      name: 'Пираты Карибского моря: Проклятие Чёрной жемчужины',
-      duration: '2 часа 23 минуты',
-      like: false,
-    }
-  ];
 
   const toggleCardLike = () => console.log('toggleCardLike');
 
---
+++ src/utils/cardList.js
@@ -0,0 +1,131 @@
+import pic from '../images/posters/pic__COLOR_pic.png';
+import pic1 from '../images/posters/pic__COLOR_pic-1.png';
+import pic2 from '../images/posters/pic__COLOR_pic-2.png';
+import pic3 from '../images/posters/pic__COLOR_pic-3.png';
+import pic4 from '../images/posters/pic__COLOR_pic-4.png';
+import pic5 from '../images/posters/pic__COLOR_pic-5.png';
+import pic6 from '../images/posters/pic__COLOR_pic-6.png';
+import pic7 from '../images/posters/pic__COLOR_pic-7.png';
+import pic8 from '../images/posters/pic__COLOR_pic-8.png';
+import pic9 from '../images/posters/pic__COLOR_pic-9.png';
+import pic10 from '../images/posters/pic__COLOR_pic-10.png';
+import pic11 from '../images/posters/pic__COLOR_pic-11.png';
+import pic12 from '../images/posters/pic__COLOR_pic-12.png';
+import pic13 from '../images/posters/pic__COLOR_pic-13.png';
+import pic14 from '../images/posters/pic__COLOR_pic-14.png';
+import pic15 from '../images/posters/pic__COLOR_pic-15.png';
+
+export const cardList = [
+  {
+    _id: 1,
+    poster: pic,
+    name: '33 слова о дизайне',
+    duration: '1ч 42м',
+    like: true,
+  },
+  {
+    _id: 2,
+    poster: pic1,
+    name: 'Киноальманах «100 лет дизайна»',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 3,
+    poster: pic2,
+    name: 'В погоне за Бенкси',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 4,
+    poster: pic3,
+    name: 'Баския: Взрыв реальности',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 5,
+    poster: pic4,
+    name: 'Бег это свобода',
+    duration: '1ч 42м',
+    like: true,
+  },
+  {
+    _id: 6,
+    poster: pic5,
+    name: 'Книготорговцы',
+    duration: '1ч 42м',
+    like: true,
+  },
+  {
+    _id: 7,
+    poster: pic6,
+    name: 'Когда я думаю о Германии ночью',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 8,
+    poster: pic7,
+    name: 'Gimme Danger: История Игги и The Stooges',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 9,
+    poster: pic8,
+    name: 'Дженис: Маленькая девочка грустит',
+    duration: '1ч 42м',
+    like: true,
+  },
+  {
+    _id: 10,
+    poster: pic9,
+    name: 'Соберись перед прыжком',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 11,
+    poster: pic10,
+    name: 'Пи Джей Харви: A dog called money',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 12,
+    poster: pic11,
+    name: 'По волнам: Искусство звука в кино',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 13,
+    poster: pic12,
+    name: 'Рудбой',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 14,
+    poster: pic13,
+    name: 'Скейт — кухня',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 15,
+    poster: pic14,
+    name: 'Война искусств',
+    duration: '1ч 42м',
+    like: false,
+  },
+  {
+    _id: 16,
+    poster: pic15,
+    name: 'Зона',
+    duration: '1ч 42м',
+    like: true,
+  },
+];
