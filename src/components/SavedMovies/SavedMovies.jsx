4b22eec392a258f65b7cd21b737a88366a52c249
Ivan Krylov <kis.226@yandex.ru>
2023-08-20 13:35:10 +0300

    add SavedMovies

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,5 +1,25 @@
+import SectionContainer from '../../components/SectionContainer/SectionContainer';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCard from '../MoviesCard/MoviesCard';
+import { cardList } from '../../utils/cardList';
+
 export default function SavedMovies() {
+
+  const deleteCard = () => console.log('deleteCard');
+
   return (
-    <section className="saved-movies"></section>
+    <main className="main">
+      <SearchForm />
+      <SectionContainer type="type_movies-list">
+        <ul className="list movies-list__list">
+          {cardList.map((card) => (
+            <MoviesCard key={card._id}
+              card={card}
+              onBtnClick={deleteCard}
+            />
+          ))}
+        </ul>
+      </SectionContainer>
+    </main>
   )
 }
