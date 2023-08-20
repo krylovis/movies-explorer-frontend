3e7487254ab1295c7e5bddfde31779e9542541d3
Ivan Krylov <kis.226@yandex.ru>
2023-08-20 13:52:53 +0300

    fix card button

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -5,6 +5,8 @@ export default function MoviesCard({ card, onBtnClick }) {
   const { pathname } = useLocation();
   const isMovies = pathname === '/movies';
 
+  const likeBtnClass = `button movies-card__button movies-card__button_type_like ${like ? 'active' : ''}`;
+  const deleteBtnClass = `button  movies-card__button movies-button_type_delete`;
   const btnLikeText = 'Поставить/убрать лайк';
   const btnLDeleteText = 'Удалить фильм из списка';
   const btnTitle = `${isMovies ? btnLikeText : btnLDeleteText}`;
@@ -15,7 +17,7 @@ export default function MoviesCard({ card, onBtnClick }) {
       <div className="movies-card__container">
         <h3 className="movies-card__title">{name}</h3>
         <button
-          className={`button movies-card__button ${like ? 'active' : ''}`}
+          className={isMovies ? likeBtnClass : deleteBtnClass}
           type="button"
           aria-label={btnTitle}
           title={btnTitle}
