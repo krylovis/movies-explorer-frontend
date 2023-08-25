95ec32f5a43280188889186ccccb09d6139f42c7
Krylov Ivan <krylovis@mchs.ru>
2023-08-25 12:31:31 +0300

    add regex

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
 export const emailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}';
+
+export const regexForEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/i;
+export const regexForName = /^[a-zа-яё -]+/i;
+export const regexForQuery = /^[a-zа-яё -\d]+/i;
