a0affd30fc9272463a0498015e57699c79d262c0
Krylov Ivan <krylovis@mchs.ru>
2023-08-25 12:30:18 +0300

    add getMoviesApi and MoviesContext

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import { MoviesContext } from '../../contexts/MoviesContext';
 import { tokenVerification } from '../../utils/Auth';
 import { mainApi } from '../../utils/MainApi';
+import { getMoviesApi } from '../../utils/MoviesApi';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import Main from '../Main/Main';
@@ -13,6 +15,7 @@ import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 export default function App() {
   const [currentUser, setCurrentUser] = React.useState({ name: '', email: '' });
+  const [moviesList, setMoviesList] = React.useState([]);
 
   const navigate = useNavigate();
 
@@ -25,6 +28,14 @@ export default function App() {
       .editUserInfo(userInfo)
       .then(setCurrentUser)
       .catch(console.error);
+  };
+
+  function getAndSetMovies() {
+    if (!moviesList.length) {
+      getMoviesApi()
+        .then(setMoviesList)
+        .catch(console.error);
+    }
   };
 
   React.useEffect(() => {
@@ -50,26 +61,29 @@ export default function App() {
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
-      <Routes>
+      <MoviesContext.Provider value={moviesList}>
+        <Routes>
 
-        <Route
-          path="/*"
-          element={<ProtectedRoute
-            loggedIn={loggedIn}
-            element={() => (
-              <Main
-                loggedIn={loggedIn}
-                setCurrentUser={setCurrentUser}
-                onUpdateUser={onUpdateUser}
-                handleSetLoggedOut={handleSetLoggedOut}
-              />
-            )} />} />
+          <Route
+            path="/*"
+            element={<ProtectedRoute
+              loggedIn={loggedIn}
+              element={() => (
+                <Main
+                  loggedIn={loggedIn}
+                  getAndSetMovies={getAndSetMovies}
+                  setCurrentUser={setCurrentUser}
+                  onUpdateUser={onUpdateUser}
+                  handleSetLoggedOut={handleSetLoggedOut}
+                />
+              )} />} />
 
-        <Route exact path='/signup' element={<SignupPage />} />
-        <Route exact path='/signin' element={<SigninPage handleSetLoggedIn={handleSetLoggedIn} setCurrentUser={setCurrentUser} />} />
+          <Route exact path='/signup' element={<SignupPage />} />
+          <Route exact path='/signin' element={<SigninPage handleSetLoggedIn={handleSetLoggedIn} setCurrentUser={setCurrentUser} />} />
 
-        <Route exact path='*' element={<NotFoundPage />} />
-      </Routes>
+          <Route exact path='*' element={<NotFoundPage />} />
+        </Routes>
+      </MoviesContext.Provider>
     </CurrentUserContext.Provider>
   );
 }
--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,1 +1,21 @@
-// const BASE_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+const BASE_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+
+const headers = {
+  'Content-Type': 'application/json'
+};
+
+const request = (url, options) => {
+  return fetch(`${BASE_URL}/${url}`, options).then(getResponse)
+};
+
+const getResponse = (res) => {
+  if (res.ok) return res.json();
+  return Promise.reject(`Ошибка: ${res.status}`);
+};
+
+export const getMoviesApi = () => {
+  return request('', {
+    method: 'GET',
+    headers,
+  })
+};
