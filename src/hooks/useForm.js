b9f195e9c373ac61a0df1733e265411817b79e1c
Krylov Ivan <krylovis@mchs.ru>
2023-08-25 12:31:46 +0300

    add useFormWithValidator

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { regexForEmail, regexForName, regexForQuery } from '../utils/constants';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 export function useForm(inputValues = {}) {
   const [values, setValues] = React.useState(inputValues);
 
   const handleChange = (event) => {
     const { value, name } = event.target;
+    console.log('');
     setValues({ ...values, [name]: value });
   };
+  return { values, handleChange, setValues };
+};
+
+export function useFormWithValidator(inputValues = {}) {
+  const [values, setValues] = React.useState(inputValues);
+  const [error, setError] = React.useState(inputValues);
+  const [isValid, setIsValid] = React.useState(false);
+
+  const handleChange = (event) => {
+    const { value, name, type } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const currentUser = React.useContext(CurrentUserContext);
+
   return { values, handleChange, setValues };
 };
