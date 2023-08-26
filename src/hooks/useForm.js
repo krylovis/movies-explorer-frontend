d253705b379b65371c208199a2c8a5a055498f2e
Ivan Krylov <kis.226@yandex.ru>
2023-08-26 11:45:20 +0300

    fix

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -7,7 +7,6 @@ export function useForm(inputValues = {}) {
 
   const handleChange = (event) => {
     const { value, name } = event.target;
-
     setValues({ ...values, [name]: value });
   };
   return { values, handleChange, setValues };
