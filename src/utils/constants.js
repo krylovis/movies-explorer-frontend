15acd8463cbd105ec780d5233df6c522e5332266
Ivan Krylov <kis.226@yandex.ru>
2023-08-26 09:08:48 +0300

    fix emailPattern

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 export const MAIN_BASE_URL = 'http://localhost:3002';
 export const MOVIES_BASE_URL = 'https://api.nomoreparties.co';
 
-export const emailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}';
+export const emailPattern = '[a-z0-9._%+\\-]+@[a-z0-9.\\-]+.[a-z]{2,4}';
 
 export const regexForEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/i;
 export const regexForName = /^[a-zа-яё -]+/i;
