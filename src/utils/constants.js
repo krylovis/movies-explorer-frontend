6b2cab90ffddcc2a7ca94d397bcae473b154714b
Ivan Krylov <kis.226@yandex.ru>
2023-08-29 01:36:24 +0300

    fix url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-// export const MAIN_BASE_URL = 'https://api.movies.krylovis.nomoreparties.sbs';
-export const MAIN_BASE_URL = 'http://localhost:3002';
+export const MAIN_BASE_URL = 'https://api.movies.krylovis.nomoreparties.sbs';
 export const MOVIES_BASE_URL = 'https://api.nomoreparties.co';
 
 export const emailPattern = '[a-z0-9._%+\\-]+@[a-z0-9.\\-]+.[a-z]{2,4}';
