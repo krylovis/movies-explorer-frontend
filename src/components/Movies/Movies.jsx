996ac6b2e3911c9b08b32c61b2fde0b63a2b46ff
Krylov Ivan <krylovis@mchs.ru>
2023-08-31 10:47:44 +0300

    add resize for card list

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -20,27 +20,32 @@ export default function Movies({ isSavedMovies }) {
   const [partOfMoviesList, setPartOfMoviesList] = React.useState([]);
   const [defaultMoviesCounter, setDefaultMoviesCounter] = React.useState(0);
 
-  const { clientWidth } = document.body;
-  const isLargeScreen = clientWidth > 990;
-  const isMediumScreen = clientWidth <= 990 && clientWidth > 768;
-  const isSmallScreen = clientWidth <= 768 && clientWidth > 500;
-  const issMobileScreen = clientWidth <= 500;
-
   React.useEffect(() => {
-    if (isLargeScreen) {
-      setDefaultMoviesCounter(16);
-      setHowMuchToAdd(4);
-    } else if (isMediumScreen) {
-      setDefaultMoviesCounter(12);
-      setHowMuchToAdd(3);
-    } else if (isSmallScreen) {
-      setDefaultMoviesCounter(8);
-      setHowMuchToAdd(2);
-    } else if (issMobileScreen) {
-      setDefaultMoviesCounter(5);
-      setHowMuchToAdd(2);
-    }
-  }, [setDefaultMoviesCounter, setHowMuchToAdd, isLargeScreen, isMediumScreen, isSmallScreen, issMobileScreen]);
+    const resize = () => {
+      const { clientWidth } = document.body;
+      const isLargeScreen = clientWidth > 990;
+      const isMediumScreen = clientWidth <= 990 && clientWidth > 768;
+      const isSmallScreen = clientWidth <= 768 && clientWidth > 500;
+      const issMobileScreen = clientWidth <= 500;
+
+      if (isLargeScreen) {
+        setDefaultMoviesCounter(16);
+        setHowMuchToAdd(4);
+      } else if (isMediumScreen) {
+        setDefaultMoviesCounter(12);
+        setHowMuchToAdd(3);
+      } else if (isSmallScreen) {
+        setDefaultMoviesCounter(8);
+        setHowMuchToAdd(2);
+      } else if (issMobileScreen) {
+        setDefaultMoviesCounter(5);
+        setHowMuchToAdd(2);
+      }
+    };
+    resize();
+    window.addEventListener('resize', resize);
+    return () => window.removeEventListener("resize", resize);
+  }, [setDefaultMoviesCounter, setHowMuchToAdd]);
 
   const getAndSetMovies = () => {
     if (!moviesList.length && !partOfMoviesList.length) {
