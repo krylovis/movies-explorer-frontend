0b9885116037a569e7fcec0595388e5125988dfe
Ivan Krylov <kis.226@yandex.ru>
2023-09-02 00:03:09 +0300

    fix emailPattern

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const MAIN_BASE_URL = 'https://api.movies.krylovis.nomoreparties.sbs';
 export const MOVIES_BASE_URL = 'https://api.nomoreparties.co';
 
-export const emailPattern = "[a-z0-9!#$%&'*+/=?^_`{|}~\\-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~\\-]+)*@(?:[a-z0-9](?:[a-z0-9\\-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9\\-]*[a-z0-9])";
+export const emailPattern = "^[a-z0-9._%+\\-]+@[a-z0-9.\\-]+\\.[a-z]{2,4}$";
 
 export const regexForName = /^[a-zа-яё -]+/i;
 export const regexForQuery = /^[a-zа-яё -\d]+/i;
